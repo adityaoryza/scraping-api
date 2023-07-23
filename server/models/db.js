ff1a30e6476ddb31f9d3b7b66b537bfed01d9b10
adityaoryza <oryza429@gmail.com>
2023-07-23 12:30:51 +0700

    update

--- server/models/db.js
+++ server/models/db.js
@@ -1,20 +1,24 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 //mongodb URI
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/ScrapingApiDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
-db.on("error", (error) => {
-  console.error("MongoDB connection error:", error);
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
 });
 
-db.once("open", () => {
-  console.log("Connected to MongoDB");
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
 
 module.exports = mongoose;
