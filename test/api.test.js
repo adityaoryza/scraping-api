d46760aaef146717775735fac0e7c851b65b0f00
adityaoryza <oryza429@gmail.com>
2023-07-23 12:08:36 +0700

    update configuration

--- test/api.test.js
+++ test/api.test.js
@@ -1,35 +1,35 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../server/app");
-const Data = require("../database/data.js");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server');
+const Data = require('../models/data.js');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe("API Endpoint Tests", () => {
+describe('API Endpoint Tests', () => {
   //note  Unit test for => GET /api/indexing
-  describe("GET /api/indexing", () => {
-    it("should scrape and index data for the current date", function (done) {
+  describe('GET /api/indexing', () => {
+    it('should scrape and index data for the current date', function (done) {
       this.timeout(10000);
 
       chai
         .request(app)
-        .get("/api/indexing")
+        .get('/api/indexing')
         .end(async (err, res) => {
           expect(res).to.have.status(200);
           expect(res.body)
-            .to.have.property("message")
+            .to.have.property('message')
             .satisfy((message) => {
               return (
-                message === "Scraping and indexing completed" ||
-                message === "Data for the current date already exists"
+                message === 'Scraping and indexing completed' ||
+                message === 'Data for the current date already exists'
               );
             });
 
-          if (res.body.message === "Scraping and indexing completed") {
+          if (res.body.message === 'Scraping and indexing completed') {
             // Check if the data is pushed to the database
-            const currentDate = new Date().toISOString().split("T")[0];
+            const currentDate = new Date().toISOString().split('T')[0];
             const data = await Data.findOne({ date: currentDate });
             expect(data).to.not.be.null;
           }
@@ -40,64 +40,64 @@ describe("API Endpoint Tests", () => {
   });
 
   //note  Unit test for => GET /api/kurs
-  describe("GET /api/kurs", () => {
-    it("should return records for the specified date range", (done) => {
-      const startdate = new Date().toISOString().split("T")[0]; //note Must be Replace with a valid date in database
-      const enddate = new Date().toISOString().split("T")[0]; //note Must be Replace with a valid date in database
+  describe('GET /api/kurs', () => {
+    it('should return records for the specified date range', (done) => {
+      const startdate = new Date().toISOString().split('T')[0]; //note Must be Replace with a valid date in database
+      const enddate = new Date().toISOString().split('T')[0]; //note Must be Replace with a valid date in database
 
       chai
         .request(app)
-        .get("/api/kurs")
+        .get('/api/kurs')
         .query({ startdate, enddate })
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.be.an("array");
+          expect(res.body).to.be.an('array');
           done();
         });
     });
 
-    it("should return 404 if no records found for the specified date range", (done) => {
-      const startdate = "2002-06-01"; //note Must be Replace with a valid date in database
-      const enddate = "2002-06-30"; //note Must be Replace with a valid date in database
+    it('should return 404 if no records found for the specified date range', (done) => {
+      const startdate = '2002-06-01'; //note Must be Replace with a valid date in database
+      const enddate = '2002-06-30'; //note Must be Replace with a valid date in database
 
       chai
         .request(app)
-        .get("/api/kurs")
+        .get('/api/kurs')
         .query({ startdate, enddate })
         .end((err, res) => {
           expect(res).to.have.status(404);
           expect(res.body)
-            .to.have.property("error")
-            .that.includes("No records found");
+            .to.have.property('error')
+            .that.includes('No records found');
           done();
         });
     });
 
-    it("should return 500 if an error occurs during invalid-date format", (done) => {
+    it('should return 500 if an error occurs during invalid-date format', (done) => {
       // Simulating an error by passing an invalid date range
-      const startdate = "invalid-date";
-      const enddate = new Date().toISOString().split("T")[0];
+      const startdate = 'invalid-date';
+      const enddate = new Date().toISOString().split('T')[0];
 
       chai
         .request(app)
-        .get("/api/kurs")
+        .get('/api/kurs')
         .query({ startdate, enddate })
         .end((err, res) => {
           expect(res).to.have.status(500);
           expect(res.body)
-            .to.have.property("error")
-            .that.equals("Invalid date format");
+            .to.have.property('error')
+            .that.equals('Invalid date format');
           done();
         });
     });
   });
 
   //note  Unit test for => GET /api/kurs/:symbol
-  describe("GET /api/kurs/:symbol", () => {
-    it("should return records for the specified symbol and date range", (done) => {
-      const symbol = "USD"; //note Must be replace with a valid symbol in dataset
-      const startdate = new Date().toISOString().split("T")[0]; //note Must be Replace with a valid date in database
-      const enddate = new Date().toISOString().split("T")[0]; //note Must be Replace with a valid date in database
+  describe('GET /api/kurs/:symbol', () => {
+    it('should return records for the specified symbol and date range', (done) => {
+      const symbol = 'USD'; //note Must be replace with a valid symbol in dataset
+      const startdate = new Date().toISOString().split('T')[0]; //note Must be Replace with a valid date in database
+      const enddate = new Date().toISOString().split('T')[0]; //note Must be Replace with a valid date in database
 
       chai
         .request(app)
@@ -105,15 +105,15 @@ describe("API Endpoint Tests", () => {
         .query({ startdate, enddate })
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.be.an("array");
+          expect(res.body).to.be.an('array');
           done();
         });
     });
 
-    it("should return 404 if no records found for the specified symbol and date range", (done) => {
-      const symbol = "KKK"; //note Must be replace with in-valid symbol in dataset
-      const startdate = "2003-05-29"; //note Must be Replace with a valid date in database
-      const enddate = "2003-05-31"; //note Must be Replace with a valid date in database
+    it('should return 404 if no records found for the specified symbol and date range', (done) => {
+      const symbol = 'KKK'; //note Must be replace with in-valid symbol in dataset
+      const startdate = '2003-05-29'; //note Must be Replace with a valid date in database
+      const enddate = '2003-05-31'; //note Must be Replace with a valid date in database
 
       chai
         .request(app)
@@ -122,18 +122,18 @@ describe("API Endpoint Tests", () => {
         .end((err, res) => {
           expect(res).to.have.status(404);
           expect(res.body)
-            .to.have.property("error")
+            .to.have.property('error')
             .that.equals(
-              "No records found for the specified symbol and date range"
+              'No records found for the specified symbol and date range'
             );
           done();
         });
     });
 
-    it("should return a CastError if an invalid date format is provided", (done) => {
-      const symbol = "USD";
-      const startdate = "invalid-date";
-      const enddate = "2023-05-31";
+    it('should return a CastError if an invalid date format is provided', (done) => {
+      const symbol = 'USD';
+      const startdate = 'invalid-date';
+      const enddate = '2023-05-31';
 
       chai
         .request(app)
@@ -142,18 +142,18 @@ describe("API Endpoint Tests", () => {
         .end((err, res) => {
           expect(res).to.have.status(500);
           expect(res.body)
-            .to.have.property("error")
-            .that.equals("Invalid startdate or enddate");
+            .to.have.property('error')
+            .that.equals('Invalid startdate or enddate');
           done();
         });
     });
   });
 
   //note  Unit test for => POST /api/kurs
-  describe("POST /api/kurs", () => {
-    it("should insert kurs data and return success message", (done) => {
+  describe('POST /api/kurs', () => {
+    it('should insert kurs data and return success message', (done) => {
       const kursData = {
-        symbol: "USD",
+        symbol: 'USD',
         e_rate: {
           jual: 1803.55,
           beli: 177355,
@@ -166,22 +166,22 @@ describe("API Endpoint Tests", () => {
           jual: 1803.55,
           beli: 177355,
         },
-        date: "2017-05-16",
+        date: '2017-05-16',
       };
 
       chai
         .request(app)
-        .post("/api/kurs")
+        .post('/api/kurs')
         .send(kursData)
         .end((err, res) => {
           if (err) {
-            console.error("Error inserting data:", err);
+            console.error('Error inserting data:', err);
             done(err);
           } else {
             expect(res).to.have.status(200);
             expect(res.body).to.have.property(
-              "message",
-              "Data successfully inserted"
+              'message',
+              'Data successfully inserted'
             );
             expect(res.body.data).to.deep.equal(kursData);
 
@@ -191,39 +191,39 @@ describe("API Endpoint Tests", () => {
                 done();
               })
               .catch((error) => {
-                console.error("Error deleting data:", error);
+                console.error('Error deleting data:', error);
                 done(error);
               });
           }
         });
     });
 
-    it("should return an error for incomplete data", (done) => {
+    it('should return an error for incomplete data', (done) => {
       const kursData = {
-        symbol: "USD",
-        date: "2023-05-30",
+        symbol: 'USD',
+        date: '2023-05-30',
         e_rate: 14100,
         // Missing tt_counter and bank_notes fields
       };
 
       chai
         .request(app)
-        .post("/api/kurs")
+        .post('/api/kurs')
         .send(kursData)
         .end((err, res) => {
           expect(res).to.have.status(400);
           expect(res.body).to.have.property(
-            "error",
-            "Incomplete data. Please provide symbol, date, value, e_rate, tt_counter, and bank_notes fields"
+            'error',
+            'Incomplete data. Please provide symbol, date, value, e_rate, tt_counter, and bank_notes fields'
           );
           done();
         });
     });
 
-    it("should return an error if data already exists", (done) => {
+    it('should return an error if data already exists', (done) => {
       const kursData = {
-        symbol: "USD",
-        date: new Date().toISOString().split("T")[0], //note Must be Replace with a valid date in database that exists
+        symbol: 'USD',
+        date: new Date().toISOString().split('T')[0], //note Must be Replace with a valid date in database that exists
         e_rate: 14100,
         tt_counter: 14050,
         bank_notes: 13950,
@@ -231,21 +231,21 @@ describe("API Endpoint Tests", () => {
 
       chai
         .request(app)
-        .post("/api/kurs")
+        .post('/api/kurs')
         .send(kursData)
         .end((err, res) => {
           expect(res).to.have.status(409);
-          expect(res.body).to.have.property("error", "Data already exists");
+          expect(res.body).to.have.property('error', 'Data already exists');
           done();
         });
     });
   });
 
   //note  Unit test for => PUT /api/kurs
-  describe("PUT /api/kurs", () => {
-    it("should update an existing data record", (done) => {
+  describe('PUT /api/kurs', () => {
+    it('should update an existing data record', (done) => {
       const kursData = {
-        symbol: "USD",
+        symbol: 'USD',
         e_rate: {
           jual: 1803.55,
           beli: 177355,
@@ -258,32 +258,32 @@ describe("API Endpoint Tests", () => {
           jual: 1803.55,
           beli: 177355,
         },
-        date: new Date().toISOString().split("T")[0], //note should be date that exist in the database
+        date: new Date().toISOString().split('T')[0], //note should be date that exist in the database
       };
 
       chai
         .request(app)
-        .put("/api/kurs")
+        .put('/api/kurs')
         .send(kursData)
         .end((err, res) => {
           if (err) {
-            console.error("Error updating data:", err);
+            console.error('Error updating data:', err);
             done(err);
           } else {
             expect(res).to.have.status(200);
             expect(res.body).to.have.property(
-              "message",
-              "Data successfully updated"
+              'message',
+              'Data successfully updated'
             );
-            expect(res.body.data).to.include.all.keys("__v", "_id");
+            expect(res.body.data).to.include.all.keys('__v', '_id');
             expect(res.body.data).to.deep.include({
               symbol: kursData.symbol,
               e_rate: kursData.e_rate,
               tt_counter: kursData.tt_counter,
               bank_notes: kursData.bank_notes,
               date:
-                new Date(kursData.date).toISOString().split("T")[0] +
-                "T00:00:00.000Z",
+                new Date(kursData.date).toISOString().split('T')[0] +
+                'T00:00:00.000Z',
             });
 
             done();
@@ -291,14 +291,14 @@ describe("API Endpoint Tests", () => {
         });
     });
 
-    it("should return an error if data record not found", async () => {
+    it('should return an error if data record not found', async () => {
       const currentDate = new Date();
       const randomOffset = Math.floor(Math.random() * (365 * 10)); // Generate a random number between 0 and 3650 (10 years)
       const pastDate = new Date(
         currentDate.getTime() - randomOffset * 24 * 60 * 60 * 1000
       )
         .toISOString()
-        .split("T")[0];
+        .split('T')[0];
       let existingRecord = await Data.findOne({ date: pastDate });
 
       while (existingRecord) {
@@ -307,12 +307,12 @@ describe("API Endpoint Tests", () => {
           currentDate.getTime() - randomOffset * 24 * 60 * 60 * 1000
         )
           .toISOString()
-          .split("T")[0];
+          .split('T')[0];
         existingRecord = await Data.findOne({ date: pastDate });
       }
 
       const kursData = {
-        symbol: "BBB",
+        symbol: 'BBB',
         e_rate: {
           jual: 1803.55,
           beli: 177355,
@@ -328,44 +328,44 @@ describe("API Endpoint Tests", () => {
         date: pastDate,
       };
 
-      const res = await chai.request(app).put("/api/kurs").send(kursData);
+      const res = await chai.request(app).put('/api/kurs').send(kursData);
 
       expect(res).to.have.status(404);
-      expect(res.body).to.have.property("error", "Data not found");
+      expect(res.body).to.have.property('error', 'Data not found');
     });
   });
 
   //note  Unit test for => DELETE /api/kurs/:date
-  describe("DELETE /api/kurs/:date", () => {
-    it("should delete records for the specified date", (done) => {
-      const date = new Date().toISOString().split("T")[0]; // Replace with a valid date in the database
+  describe('DELETE /api/kurs/:date', () => {
+    it('should delete records for the specified date', (done) => {
+      const date = new Date().toISOString().split('T')[0]; // Replace with a valid date in the database
 
       chai
         .request(app)
         .delete(`/api/kurs/${date}`)
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.have.property("message").that.includes(date);
+          expect(res.body).to.have.property('message').that.includes(date);
           done(); // Call done() to indicate the completion of the test
         });
     });
 
-    it("should return 404 if no records found for the specified date", (done) => {
-      const date = "2003-05-30"; //note Must be Replace with a valid date in database
+    it('should return 404 if no records found for the specified date', (done) => {
+      const date = '2003-05-30'; //note Must be Replace with a valid date in database
 
       chai
         .request(app)
         .delete(`/api/kurs/${date}`)
         .end((err, res) => {
           expect(res).to.have.status(404);
-          expect(res.body).to.have.property("error").that.includes(date);
+          expect(res.body).to.have.property('error').that.includes(date);
           done();
         });
     });
 
-    it("should return 500 if an error occurs during deletion and invalid-date", (done) => {
+    it('should return 500 if an error occurs during deletion and invalid-date', (done) => {
       // Simulating an error by passing an invalid date format
-      const date = "invalid-date";
+      const date = 'invalid-date';
 
       chai
         .request(app)
@@ -373,8 +373,8 @@ describe("API Endpoint Tests", () => {
         .end((err, res) => {
           expect(res).to.have.status(500);
           expect(res.body)
-            .to.have.property("error")
-            .that.equals("Invalid date format");
+            .to.have.property('error')
+            .that.equals('Invalid date format');
           done();
         });
     });
